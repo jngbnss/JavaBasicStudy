8e2b538b2bfacb3919aa8619a57035a7eff7bff4
jngbnss <jngbnmn@naver.com>
2025-10-01 15:06:30 +0900

    set시작

---
+++ src/javaMid2/collection/set/MyHashSetV0Main.java
@@ -0,0 +1,23 @@
+package javaMid2.collection.set;
+
+import java.util.Arrays;
+
+public class MyHashSetV0Main {
+    public static void main(String[] args) {
+        MyHashSetV0 set = new MyHashSetV0();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        System.out.println("set = " + set);
+
+        boolean result = set.add(4);
+        System.out.println("result = " + result);
+        System.out.println("set = " + set);
+
+        System.out.println("set.contains(3): " + set.contains(3)); // O(n)
+        System.out.println("set.contains(99): " + set.contains(99)); // O(n)
+
+
+    }
+}
