9886fbe75fd49fb246a30e62d7f53e9cac9f027f
jngbnss <jngbnmn@naver.com>
2025-10-15 14:33:26 +0900

    야구게임 완료 자료구조 공부

---
+++ src/mvcPractice/basic/baseball/controller/BaseballController.java
@@ -0,0 +1,37 @@
+package mvcPractice.basic.baseball.controller;
+
+import mvcPractice.basic.baseball.model.BaseballGame;
+import mvcPractice.basic.baseball.view.BaseballView;
+
+import java.util.List;
+
+public class BaseballController {
+    public static void main(String[] args) {
+        BaseballView view = new BaseballView();
+        BaseballGame game = new BaseballGame();
+
+        boolean isCorrect = false;
+
+        while(!isCorrect){
+            List<Integer>guess = view.getUserInput();
+            int [] result = game.checkGuess(guess);
+
+            int strike = result[0];
+            int ball = result[1];
+
+            view.printResult(strike,ball);
+            if(strike==4){
+                view.printWinMessage();
+                isCorrect = true;
+            }
+        }
+    }
+}
+/**
+ * 몇번 시도했는지
+ * 예외처리
+ * 정답확인
+ * 힌트확인
+ * 다시시작하시
+ *
+ */
---
+++ src/mvcPractice/basic/baseball/model/BaseballGame.java
@@ -0,0 +1,42 @@
+package mvcPractice.basic.baseball.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BaseballGame {
+    private List<Integer> answer;
+
+    public BaseballGame() {
+        generateAnswer();
+    }
+
+    private void generateAnswer() {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        answer = numbers.subList(0,4);
+    }
+    //결과 계산
+    public int[]checkGuess(List<Integer>guess){
+        int strike =0;
+        int ball = 0;
+
+        for (int i = 0; i < 4; i++) {
+            if(guess.get(i).equals(answer.get(i))){
+                strike++;
+            }else if(answer.contains(guess.get(i))){
+                ball++;
+            } // 아 이렇게하면 편하긴 하구나
+        }
+        return new int[]{strike,ball}; // 이건 무슨뜻이지?
+    }
+    public List<Integer>getAnswer(){
+        return answer;
+    }
+    // 카운트 몇번 걸리는지 알고싶은데
+
+
+}
