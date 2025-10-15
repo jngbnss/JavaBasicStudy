9de0d77e300afa5a17b0b7fe89744714b23ef145
jngbnss <jngbnmn@naver.com>
2025-10-15 14:06:56 +0900

    자바의 정석 문제풀이 및 계산기에 예외 붙임

---
+++ src/ch8/ex8_9/Exercise8_9.java
@@ -0,0 +1,7 @@
+package ch8.ex8_9;
+
+public class Exercise8_9 {
+    public static void main(String[] args) {
+        throw new UnsupportedFunctionException("지원하지 않는 기능입니다.",100);
+    }
+}
--- src/pre1/cal1.java
+++ src/pre1/cal1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 // "" => 0, "1,2" => 3, "1,2,3" => 6, "1,2:3" => 6
 public class cal1 {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String temp = "";
@@ -16,8 +17,11 @@ public class cal1 {
                     ret += Integer.parseInt(temp);
                     temp = "";
                 }
-            } else {
+            }else if(Character.isDigit(c)) {
                 temp += c;
+            }else{
+                throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+
             }
         }
 
@@ -26,7 +30,7 @@ public class cal1 {
             ret += Integer.parseInt(temp);
         }
 
-        System.out.println("ret = " + ret);
+        System.out.println("결과 : " + ret);
         // 여기 분석하고 예외하면 되나
         // 일단 읽는게 중요할듯
     }
