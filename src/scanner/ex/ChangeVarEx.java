bbcfed7d9f3a728bf1cd7c7eaa7ea6251bb20b1c
jngbn <[user email]>
2025-10-20 05:46:10 +0900

    feat:java-start-Scanner-문제와 풀이2

---
+++ src/scanner/ex/ChangeVarEx.java
@@ -0,0 +1,20 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ChangeVarEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+//        Scanner scanner = new Scanner(System.in);
+//         a = scanner.nextInt();
+        System.out.println("a = " + a);
+//        b = scanner.nextInt();
+        System.out.println("b = " + b);
+
+    }
+}
