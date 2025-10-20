b85b6c3f71972eba637af969c43dd9e88e318a71
jngbnss <jngbnmn@naver.com>
2025-10-20 09:03:45 +0900

    docs:save

---
+++ src/baekjoon/level3/ex.java
@@ -0,0 +1,15 @@
+package baekjoon.level3;
+
+public class ex {
+    public static void main(String[] args) {
+
+        int a =2;
+dsafsdaf
+
+        for(int i=0;i<10;i++){
+
+    dsdsfdfs            System.out.println(a+i);
+    }
+}
+
+}
