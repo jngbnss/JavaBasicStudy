940e5aa61c06ca44fcf495fcacce608705aa528d
jngbnss <jngbnmn@naver.com>
2025-10-30 11:25:55 +0900

    feat: java mid1 - section2 object class

--- src/javaStart/Ch9Array/array/ex/ProductAdminEx.java
+++ src/javaStart/Ch9Array/array/ex/ProductAdminEx.java
@@ -1,51 +1,44 @@
-//package javaStart.Ch9Array.array.ex;
-//
-//
-//import java.util.Scanner;
-//
-//public class ProductAdminEx {
-//    String[] productNames = new String[10];
-//    int[] productPrices = new int[10];
-//    int cnt =0;
-//
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료");
-//        System.out.print("메뉴를 선택하세요:");
-//
-//
-//        int menu = scanner.nextInt();
-//        cnt = 0;
-//        switch (menu){
-//            case 1:
-//                save();
-//                break;
-//            case 2:
-//                list();
-//                break;
-//            case 3:
-//                System.out.println("프로그램을 종료합니다.");
-//                return;
-//            default:
-//                System.out.println("잘못된 값을 선택하셨습니다.");
-//        }
-//    }
-//
-//    private static void list() {
-//        for(int i=0;i<cnt;i++){
-//            System.out.println(productNames[i]+": "+productPrices[i]+"원");
-//        }
-//    }
-//
-//    private static void save() {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("상품 이름을 입력하세요:");
-//        String name = scanner.nextLine();
-//        productNames[cnt] =name;
-//        int price = scanner.nextInt();
-//        productPrices[cnt] = price;
-//        cnt++;
-//
-//
-//    }
-//}
+package javaStart.Ch9Array.array.ex;
+
+
+import java.util.Scanner;
+
+public class ProductAdminEx {
+
+
+    public static void main(String[] args) {
+        String[] productNames = new String[10];
+        int[] productPrices = new int[10];
+        int cnt =0;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료");
+            System.out.print("메뉴를 선택하세요:");
+
+
+            int menu = scanner.nextInt();
+            scanner.nextLine(); //버퍼 지우기
+            if(menu==1) {
+                System.out.print("상품 이름을 입력하세요:");
+                String name = scanner.nextLine();
+                System.out.print("상품 가격을 입력하세요:");
+                int price = scanner.nextInt();
+                productNames[cnt] = name;
+                productPrices[cnt] = price;
+                cnt++;
+            }else if(menu==2){
+                for(int i=0;i<cnt;i++){
+                    System.out.println(productNames[i]+": "+productPrices[i]+"원");
+                }
+            }else if(menu==3){
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }else {
+                System.out.println("잘못된 값을 선택하셨습니다.");
+            }
+
+        }
+
+    }
+
+}
