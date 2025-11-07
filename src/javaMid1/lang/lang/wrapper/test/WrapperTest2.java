8f96822c3a11b659c720e6c552c3c2699a940280
jngbnss <jngbnmn@naver.com>
2025-11-07 18:28:54 +0900

    feat: 문제풀기까지 완료 이제 enum

---
+++ src/javaMid1/lang/lang/wrapper/test/WrapperTest2.java
@@ -0,0 +1,13 @@
+package javaMid1.lang.lang.wrapper.test;
+
+public class WrapperTest2 {
+    public static void main(String[] args) {
+        String[]array ={"1.5","2.5","3.0"};
+        double sum=0;
+        for (String s : array) {
+            double temp = Double.parseDouble(s);
+            sum+=temp;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
