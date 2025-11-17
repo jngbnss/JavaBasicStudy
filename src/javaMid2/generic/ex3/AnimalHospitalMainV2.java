3eb015602706a2b7795fec51455423595f9cff93
jngbn <jngbnss@naver.com>
2025-11-17 10:16:30 +0900

    feat: generic 타입 매개변수 제한4

---
+++ src/javaMid2/generic/ex3/AnimalHospitalMainV2.java
@@ -0,0 +1,11 @@
+package javaMid2.generic.ex3;
+
+import javaMid2.generic.animal.Cat;
+import javaMid2.generic.animal.Dog;
+
+public class AnimalHospitalMainV2 {
+    public static void main(String[] args) {
+        AnimalHospitalV2<Dog> dogHospital = new AnimalHospitalV2<>();
+        AnimalHospitalV2<Integer> integerHospital = new AnimalHospitalV2<>();
+    }
+}
