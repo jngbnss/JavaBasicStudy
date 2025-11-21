600186bb0c69e91979bceb54e9cd2948b009251b
jngbn <jngbnss@naver.com>
2025-11-21 10:58:32 +0900

    feat: iterator

---
+++ src/javaMid2/collection/compare/SortMain2.java
@@ -0,0 +1,11 @@
+package javaMid2.collection.compare;
+
+import java.util.Arrays;
+
+public class SortMain2 {
+    public static void main(String[] args) {
+        Integer[]array = {3,2,1};
+        System.out.println(Arrays.toString(array));
+
+    }
+}
---
+++ src/javaMid2/collection/iterable/JavaIterableMain.java
@@ -0,0 +1,39 @@
+package javaMid2.collection.iterable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+public class JavaIterableMain {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        printAll(list.iterator());
+        printAll(set.iterator());
+
+        foreach(list);
+        foreach(set);
+    }
+
+    private static void printAll(Iterator<Integer> iterator) {
+        System.out.println("iterator = " + iterator.getClass());
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    private static void foreach(Iterable<Integer> iterable) {
+        System.out.println("iterable = " + iterable.getClass());
+        for (Integer i : iterable) {
+            System.out.println(i);
+        }
+    }
+}
