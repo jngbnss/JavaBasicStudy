e2958c7398164875dd5b9a399fdb5ee732653e38
jngbnss <jngbnmn@naver.com>
2025-11-29 15:07:20 +0900

    feat: 고급 3편 섹션4. 함수형 인터페이스 제네릭 도입

---
+++ src/javaAdv3/lambda/lambda3/GenericMain1.java
@@ -0,0 +1,24 @@
+package javaAdv3.lambda.lambda3;
+
+import javaAdv3.lambda.ex2.StringFunction;
+
+public class GenericMain1 {
+    public static void main(String[] args) {
+        StringFunction upperCase = s->s.toUpperCase();
+        String result1 = upperCase.apply("hello");
+        System.out.println("result1 = " + result1);
+
+        NumberFunction square = n -> n*n;
+        Integer result2 = square.apply(3);
+        System.out.println("result2 = " + result2);
+    }
+
+    @FunctionalInterface
+    interface StringFunction{
+        String apply(String s);
+    }
+    @FunctionalInterface
+    interface NumberFunction{
+        Integer apply(Integer s);
+    }
+}
