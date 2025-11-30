3de0ad0ec1c4803e7b82e4cae53f6a6aebf26c4e
jngbn <jngbnss@naver.com>
2025-12-01 01:31:22 +0900

    feat: 고급 2편 섹션9. 네트워크 - 프로그램2

---
+++ src/javaAdv2/network/tcp/v4/SessionV4.java
@@ -0,0 +1,53 @@
+package javaAdv2.network.tcp.v4;
+
+import static javaAdv2.network.tcp.SocketCloseUtil.closeAll;
+import static javaAdv2.util.MyLogger.log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SessionV4 implements Runnable{
+    private final Socket socket;
+
+    public SessionV4(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        //finally 블록에서 변수에 접근해야 한다. 따라서 try 블록 안에서 선언할 수 없다.
+        DataInputStream input = null;
+        DataOutputStream output = null;
+
+        try{
+            input = new DataInputStream(socket.getInputStream());
+            output = new DataOutputStream(socket.getOutputStream());
+
+            while(true){
+                //클라이언트로부터 문자 받기
+                String received = input.readUTF();
+                log("client -> server: "+received);
+
+                if(received.equals("exit")){
+                    break;
+                }
+
+                //클라이언트에게 문자 보내기
+                String toSend = received+" World!";
+                output.writeUTF(toSend);
+                log("client <- server: "+toSend);
+            }
+
+            //자원 정리
+        }catch(IOException e){
+            log(e);
+        }finally {
+            closeAll(socket,input,output);
+            log("연결 종료: "+socket);
+        }
+
+
+    }
+}
